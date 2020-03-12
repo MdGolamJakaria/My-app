5cd1b52d44481c934890f6cbc901a3792654b140
Md Golam Jakaria <58362149+MdGolamJakaria@users.noreply.github.com>
2020-03-12 14:18:53 +0600

    react

--- src/App.js
+++ src/App.js
@@ -3,24 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const AboutDr = {
+    name:"Dr. Holder",
+    age: 58,
+    job:"singer",
+    Num : 200,}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+      <DoctorDetails name={AboutDr.name} age={AboutDr.age} job={AboutDr.job} LucchamiNum={AboutDr.Num}></DoctorDetails>
+
+      <DoctorDetails name={AboutDr.name} age={AboutDr.age} job={AboutDr.job} LucchamiNum={AboutDr.Num} ></DoctorDetails>
+
       </header>
     </div>
   );
+
 }
 
+function DoctorDetails(props){
+  var AboutDrStyle = {
+    color:'red',
+    backgroundColor :'White',
+    border: ' 2px solid red ',
+    borderRadius: '10px',
+    padding : '20px',
+    textAlign:'left',
+    width: '500px',
+    marginBottom:'20px',    
+  }
+
+  return (
+      <div  style={AboutDrStyle}>
+          <h1>Doctor Name : {props.name} </h1>
+          <h2>Doctor Age : {props.age} </h2>
+          <h3>Doctor Job : {props.job} </h3>
+          <h4>Doctor's Num: {props.Num} </h4>
+      </div>
+  )
+    
+}
 export default App;
